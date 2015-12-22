de0e497c1d6c8ee80b9015dfa96e59e2956bb448
Joshua Weinstein <jweinst1@berkeley.edu>
2015-12-22 10:48:58 -0800

    finished tests

--- Squidword/Unittests.py
+++ Squidword/Unittests.py
@@ -1,5 +1,7 @@
 import unittest
 from Squidword.Sentence_Trie import *
+from Squidword import Trie_Utils
+from Squidword import __main__
 
 
 
@@ -14,7 +16,42 @@ class Trie_tests(unittest.TestCase):
         box.statement("i love corn.")
         box.statement("i love chicken.")
         self.assertEqual({'i': {'love': {'chicken.': {}, 'corn.': {}}}}, box.trie)
+    def Insertion_test03(self):
+        f = strie()
+        f.statement("People don't love corn.")
+        f.statement("People do love corn.")
+        f.statement("People can love corn.")
+        f.statement("People could love corn.")
+        self.assertEqual({'People': {'do': {'love': {'corn.': {}}}, 'could': {'love': {'corn.': {}}}, 'can': {'love': {'corn.': {}}}, "don't": {'love': {'corn.': {}}}}}, f.trie)
 
+class random_statements(unittest.TestCase):
+   #tests random generation of natural phrases
+    def random_test01(self):
+        f = strie()
+        f.statement("People don't love corn.")
+        f.statement("People do love corn.")
+        f.statement("People can love corn.")
+        f.statement("People could love corn.")
+        result = Trie_Utils.random_statement(f)
+        self.assertTrue(f.istrue(result))
+    def random_test02(self):
+        f = strie()
+        f.statement("I need lettuce.")
+        f.statement("Me and samuel go to the park.")
+        f.statement("People can love corn.")
+        f.statement("People could love corn.")
+        result = Trie_Utils.random_statement(f)
+        self.assertTrue(f.istrue(result))
 
+class File_tests(unittest.TestCase):
+
+    def file_test01(self):
+        import os
+        __main__.init_squid()
+        self.assertTrue(os.path.exists(".squidword"))
+    def trie_createtest(self):
+        import os
+        __main__.new_trie("practice.txt", "firstsave")
+        self.assertTrue(os.path.exists(os.path.join(".squidword", "firstsave")))
 if __name__ == '__main__':
     unittest.main()
